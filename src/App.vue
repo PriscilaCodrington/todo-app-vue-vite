2d2690c164dbe9b2914bbac924f58a08c836d28e
PriscilaCodrington <priscilacodrington@gmail.com>
2023-05-01 00:16:19 +0200

    To-do App Complete

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ export default {
   name: "Todo App",
   data() {
     return {
+      editedTaskIndex: "",
       task: "",
       editedTaskIndex: null,
       statuses: ["to-do", "in-progress", "finished"],
@@ -20,28 +21,59 @@ export default {
           status: "finished",
         },
       ],
+      finish: false,
     };
   },
   computed: {
     shouldShowCongratulations() {
-      // write your code here
+      return this.tasks.every((task) => task.status === "finished");
     },
   },
   methods: {
     changeStatus(task) {
-      // write your code here
+      for (let i = 0; i < this.statuses.length; i++) {
+        if (task.status === this.statuses[i]) {
+          i++;
+          task.status = this.statuses[i];
+        } else if (task.status === this.statuses[2])
+          task.status = this.statuses[0];
+      }
     },
     getStatusClass(task) {
-      // write your code here
+      switch (task.status) {
+        case "to-do":
+          return "text-danger";
+          break;
+        case "in-progress":
+          return "text-warning";
+          break;
+        case "finished":
+          return "text-success";
+          break;
+        case "default":
+          break;
+      }
     },
     deleteTask(index) {
-      // write your code here
+      this.tasks.splice(index, 1);
     },
     editTask(index) {
-      // write your code here
+      this.task = this.tasks[index].name;
+      this.editedTaskIndex = index;
     },
     submitTask() {
-      // write your code here
+      if (this.editedTaskIndex === null) {
+        if (this.task.length === 0) return;
+        this.tasks.push({
+          name: this.task,
+          status: "to-do",
+        });
+      } else {
+        this.tasks[this.editedTaskIndex].name = this.task;
+        this.editedTaskIndex = null;
+      }
+
+      this.task = "";
     },
   },
 };
@@ -55,11 +87,12 @@ export default {
     <!-- Input -->
     <div class="d-flex mt-5">
       <input
-          type="text"
-          placeholder="Enter task"
-          class="w-100 form-control"
+        type="text"
+        placeholder="Enter task"
+        class="w-100 form-control"
+        v-model="task"
       />
-      <button class="btn btn-warning rounded-0">
+      <button class="btn btn-warning rounded-0" @click="submitTask">
         SUBMIT
       </button>
     </div>
@@ -67,41 +100,43 @@ export default {
     <!-- Task table -->
     <table class="table table-bordered mt-5">
       <thead>
-      <tr>
-        <th scope="col">Task</th>
-        <th scope="col" style="width: 120px">Status</th>
-        <th scope="col" class="text-center">#</th>
-        <th scope="col" class="text-center">#</th>
-      </tr>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col" style="width: 120px">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>
-            <span class="">
-              --- task name ---
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="task.status === 'finished' ? 'line-through' : ''">
+              {{ task.name }}
             </span>
-        </td>
-        <td>
+          </td>
+          <td>
             <span
-                class="pointer"
+              class="pointer"
+              :class="getStatusClass(task)"
+              @click="changeStatus(task)"
             >
-              -- task status --
+              {{ task.status }}
             </span>
-        </td>
-        <td class="text-center">
-          <div>
-            <span class="fa fa-trash pointer"/>
-          </div>
-        </td>
-        <td class="text-center">
-          <div>
-            <span class="fa fa-pen pointer"/>
-          </div>
-        </td>
-      </tr>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer" />
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <span class="fa fa-pen pointer" />
+            </div>
+          </td>
+        </tr>
       </tbody>
     </table>
-    <div>
+    <div v-if="shouldShowCongratulations">
       Congratulations! You completed all your tasks!
     </div>
   </div>
@@ -109,7 +144,7 @@ export default {
 
 <style>
 body {
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 }
 
 .pointer {
